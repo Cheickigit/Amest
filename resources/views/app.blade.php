fc24ea7ad991c4f915b106a212cc07cd63051f68
bahamasangare <bahamasangare99@gmail.com>
2025-08-29 13:06:23 +0100

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title inertia>{{ config('app.name', 'BK Construction') }}</title>
+    <title inertia>{{ config('app.name', 'BKO Construction') }}</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,600,700,800|teko:500,600&display=swap" rel="stylesheet" />
